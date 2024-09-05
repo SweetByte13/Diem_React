59b310217f04833a1cb870d70dc5eb63a362c8a6
Tova Yocheved Hillman <hillman.t.y@gmail.com>
2024-09-05 12:01:37 -0400

    signup and login buttons navigating to pages from navbar. login form finished, working on form submission.

--- diemClient/src/components/Footer.js
+++ diemClient/src/components/Footer.js
@@ -5,21 +5,21 @@ function Footer() {
     const useAppContext = () => useContext(AppContext);
     const { user, setUser } = useAppContext();
     return (
-        <footer class="w-full py-14">
-            <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                <div class="max-w-3xl mx-auto">
-                    <a  class="flex justify-center">
-                            <img src="./favicon.ico" class="size-36 flex justify-center rounded-full"/>
+        <footer className="w-full py-14">
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div className="max-w-3xl mx-auto">
+                    <a  className="flex justify-center">
+                            <img src="./favicon.ico" className="size-36 flex justify-center rounded-full"/>
                     </a>
-                    <ul class="text-lg flex items-center justify-center flex-col gap-7 md:flex-row md:gap-12 transition-all duration-500 py-16 mb-10 border-b border-gray-200">
-                        <li><a  class="text-gray-800 hover:text-gray-900">Home</a></li>
-                        {user === null || user === undefined ? <li><a class=" text-gray-800 hover:text-gray-900">Sign Up</a></li> : ""}
-                        {user === null || user === undefined ? "" : <li><a class=" text-gray-800 hover:text-gray-900">Habits</a></li>}
-                        {user === null || user === undefined ? "" : <li><a class=" text-gray-800 hover:text-gray-900">Calender</a></li>}
-                        {user === null || user === undefined ? "" : <li><a class=" text-gray-800 hover:text-gray-900">Account</a></li>}
-                        <li><a  class=" text-gray-800 hover:text-gray-900">Support</a></li>
+                    <ul className="text-lg flex items-center justify-center flex-col gap-7 md:flex-row md:gap-12 transition-all duration-500 py-16 mb-10 border-b border-gray-200">
+                        <li><a  className="text-gray-800 hover:text-gray-900">Home</a></li>
+                        {user === null || user === undefined ? <li><a className=" text-gray-800 hover:text-gray-900">Sign Up</a></li> : ""}
+                        {user === null || user === undefined ? "" : <li><a className=" text-gray-800 hover:text-gray-900">Habits</a></li>}
+                        {user === null || user === undefined ? "" : <li><a className=" text-gray-800 hover:text-gray-900">Calender</a></li>}
+                        {user === null || user === undefined ? "" : <li><a className=" text-gray-800 hover:text-gray-900">Account</a></li>}
+                        <li><a  className=" text-gray-800 hover:text-gray-900">Support</a></li>
                     </ul>
-                    <span class="text-lg text-gray-500 text-center block">© Diem 2024, All rights reserved.</span>
+                    <span className="text-lg text-gray-500 text-center block">© Diem 2024, All rights reserved.</span>
                 </div>
             </div>
         </footer>
